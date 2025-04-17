ca86648f3a00a7b7b92f1b5142290ffd81d1d3f4
MinEquals0 <minequals0@gmail.com>
2025-04-17 12:06:11 +0900

    feat: create entity package, User and Authority class
    

---
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/entity/Authority.java
@@ -0,0 +1,22 @@
+package me.minequals0.tutorial.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authoriy_name", length = 50)
+    private String authorityName;
+
+}
