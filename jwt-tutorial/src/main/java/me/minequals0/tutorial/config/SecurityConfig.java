1c477453af499651e7b4c1242a78ed91b38df346
MinEquals0 <minequals0@gmail.com>
2025-04-18 22:15:45 +0900

    fix: switch enable h2-console logic

--- jwt-tutorial/src/main/java/me/minequals0/tutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package me.minequals0.tutorial.config;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,12 +19,21 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((registry) ->
-                        registry.requestMatchers("/api/hello").permitAll()
-                                .anyRequest().authenticated()
+//                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // ✅ iframe 허용
+//                .authorizeHttpRequests((registry) ->
+//                        registry.requestMatchers("/api/hello", "/h2-console/**").permitAll()
+//                                .anyRequest().authenticated() // 예전 강의에 가까운 코드
+                // 24년도 수정된 코드
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/api/hello","/h2-console/**").permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                // enable h2-console
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                 )
                 .build();
     }
-
-
 }
