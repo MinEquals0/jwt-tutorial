4346d344222fcaa6b1618c2d5393f497f8a79eb7
MinEquals0 <minequals0@gmail.com>
2025-04-19 12:10:00 +0900

    feat: create JwtFilter

---
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/jwt/JwtFilter.java
@@ -0,0 +1,62 @@
+package me.minequals0.tutorial.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+// 주로 요청(Request)에 포함된 JWT 토큰 검사,검증
+// 인증된 사용자 정보를 SecurityContext에 설정
+// HTTP 요청이 들어올 때마다 자동으로 실행되며, 주로 보안 관련 작업을 수행
+public class JwtFilter extends GenericFilterBean {
+    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    // 실제 필터링 로직은 doFilter 내부에 작성
+    // JWT 토큰의 인증 정보를 현재 실행 중인 SecurityContext에 저장하는 역할
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        // request에서 토큰을 받아서 jwt에 저장
+        String jwt = resolveToken(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
+
+        // jwt(토큰)이 유효성 검사를 통과하면(정상토큰) 유저 정보를 가져와서
+        // SecurityContext에 저장
+        if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)){
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}",
+                    authentication.getName(), requestURI );
+        } else {
+            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    // Request Header에서 토큰 정보를 꺼내오기 위한 resolveToken 메소드
+    private String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
