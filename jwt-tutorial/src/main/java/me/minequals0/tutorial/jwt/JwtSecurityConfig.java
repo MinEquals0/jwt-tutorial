cb34b9fb917f858404220d0df5e32b595454d3fd
MinEquals0 <minequals0@gmail.com>
2025-04-19 12:16:00 +0900

    feat: create JwtSecurityConfig

---
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,26 @@
+package me.minequals0.tutorial.jwt;
+
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// TokenProvider, JwtFilter를 SecurityConfig에 적용할 때 사용
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private TokenProvider tokenProvider;
+
+    //TokenProvider DI
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    //JwtFilter를 통해 Security 로직에 필터를 등록
+    @Override
+    public void configure(HttpSecurity http) {
+        http.addFilterBefore(
+                new JwtFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
