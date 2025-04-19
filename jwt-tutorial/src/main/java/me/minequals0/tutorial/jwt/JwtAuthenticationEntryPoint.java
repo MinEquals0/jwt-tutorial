4eb0efb8bc051c3a4ddfbc4ae032ab97bf19b354
MinEquals0 <minequals0@gmail.com>
2025-04-19 12:18:37 +0900

    feat: create JwtAuthenticationEntryPoint

---
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package me.minequals0.tutorial.jwt;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+// 유효한 자격 증명을 제공하지 않고 접근하려 할 때, 401 Unauthorized 에러를 리턴하는 역할
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+
+    }
+}
