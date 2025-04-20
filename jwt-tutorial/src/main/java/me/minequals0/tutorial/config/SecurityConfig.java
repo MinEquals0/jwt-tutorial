7dbb339450b8a67c3025dd9f420ca557acdcc8f3
MinEquals0 <minequals0@gmail.com>
2025-04-20 12:53:39 +0900

    feat: update SecurityConfig with exception handling and JWT config

--- jwt-tutorial/src/main/java/me/minequals0/tutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.minequals0.tutorial.config;
 
 
+import me.minequals0.tutorial.jwt.*;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,31 +10,70 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    // 생성자, DI
+    public SecurityConfig(TokenProvider tokenProvider,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    // ??
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 토큰을 사용하니까 csrf는 disable
                 .csrf(AbstractHttpConfigurer::disable)
-//                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // ✅ iframe 허용
-//                .authorizeHttpRequests((registry) ->
-//                        registry.requestMatchers("/api/hello", "/h2-console/**").permitAll()
-//                                .anyRequest().authenticated() // 예전 강의에 가까운 코드
-                // 24년도 수정된 코드
+
+                //추가, 이후 수정
+//                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                    .accessDeniedHandler(jwtAccessDeniedHandler)
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        )
+
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        .requestMatchers("/api/hello","/h2-console/**").permitAll()
+                        .requestMatchers("/api/hello",
+                                "/h2-console/**",  //8080으로 DB 확인용
+                                "/api/authenticate", // 로그인 API
+                                "/api/signup").permitAll() // 회원가입 API
+                        // 위의 API들은 토큰이 없는 상태에서 요청이 들어오기 때문에 모두 permitAll 설정
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .anyRequest().authenticated()
                 )
 
-                // enable h2-console
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정 - 추가
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                // h2-console을 위한 설정
                 .headers(headers ->
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                 )
-                .build();
+
+
+                .with(new JwtSecurityConfig(tokenProvider), customizer -> {});
+        return http.build();
     }
 }
