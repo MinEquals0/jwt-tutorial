be9fec3400026317191dcc2bc16c066e01294240
MinEquals0 <minequals0@gmail.com>
2025-04-20 12:54:05 +0900

    fix: add annotation

--- jwt-tutorial/src/main/java/me/minequals0/tutorial/jwt/JwtAuthenticationEntryPoint.java
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 // 유효한 자격 증명을 제공하지 않고 접근하려 할 때, 401 Unauthorized 에러를 리턴하는 역할
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
