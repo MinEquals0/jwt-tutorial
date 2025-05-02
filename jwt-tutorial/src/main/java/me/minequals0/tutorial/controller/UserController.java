e1b87ffdb396b0c3a72052f39845087ec5618b5c
MinEquals0 <minequals0@gmail.com>
2025-05-02 16:43:18 +0900

    feat: create UserController

---
+++ jwt-tutorial/src/main/java/me/minequals0/tutorial/controller/UserController.java
@@ -0,0 +1,18 @@
+package me.minequals0.tutorial.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// JWT 토큰을 로그인 응답에서 추출해 Postman의 Scripts에 저장
+// 다른 보호된 API 요청에 자동으로 붙여서 인증 테스트하기
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloUser() {
+        return ResponseEntity.ok("Hello, authenticated user!");
+    }
+}
